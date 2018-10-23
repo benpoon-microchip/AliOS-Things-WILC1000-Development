a7082c3d9fdeb8f99f9fc4ac3bf56f1d8056cf12
Ben Poon <Ben.Poon@microchip.com>
2018-10-23 17:04:24 +0800

    add wilc host driver

--- device/sal/sal.mk
+++ device/sal/sal.mk
@@ -17,4 +17,6 @@ else ifeq (gprs.sim800,$(module))
 $(NAME)_COMPONENTS += sal.gprs.sim800
 else ifeq (wifi.esp8266,$(module))
 $(NAME)_COMPONENTS += sal.wifi.esp8266
+else ifeq (wifi.wilc1000,$(module))
+$(NAME)_COMPONENTS += sal.wifi.wilc1000
 endif
